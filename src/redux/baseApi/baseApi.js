969bb8b7a146540e0db1b4b6b1874d3703c0bd5d
nimurr <nimurnerob404@gmail.com>
2026-01-10 09:38:39 +0600

    `Removed trailing whitespace at end of file`

--- src/redux/baseApi/baseApi.js
+++ src/redux/baseApi/baseApi.js
@@ -18,4 +18,3 @@ export const baseApi = createApi({
   tagTypes: ["User", "Categories", "Babucare", "ComboBox", "Products", "BuildBox", 'User-2', "Subscription", "Setting", 'Privacy-Policy', "Profile", "Document", "Lawyer"],
   endpoints: () => ({}),
 });
- 
